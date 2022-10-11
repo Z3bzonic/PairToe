f3d7daec6ccce3b4ec5c73d36decc76a7776593b
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-10-11 13:15:11 +0200

    Add files via upload

---
+++ PairToe.Business/GameEngine.cs
@@ -0,0 +1,24 @@
+﻿using PairToe.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairToe.Business
+{
+    internal class GameEngine
+    {
+        public State state
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+            }
+        }
+    }
+}
