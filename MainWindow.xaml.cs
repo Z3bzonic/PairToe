e787f4cc200a0a122864a5bad0ffe45655a2f310
Bart Roels <roelsbart@gmail.com>
2022-10-11 12:37:32 +0200

    muhaha

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using PairToe.Common.Enum;
+using PairToe.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -18,11 +24,53 @@ namespace PairToe
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public MainViewModel vm { get; private set; } = new MainViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+            Gridke.DataContext = vm;
+        }
+
+        public State state { get; set; } = State.O;
+
+        public string[] gameBoard = new string[9];
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            currentplayer.Content = state.ToString();
+            Button b = sender as Button;
+            b.Content = state.ToString();
+            b.IsEnabled = false;
+            SwitchState();
+            int tilenumber;
+            bool tile = int.TryParse(Regex.Replace(b.Name, "[^0-9]+", string.Empty), out tilenumber);
+            gameBoard[tilenumber - 1] = state.ToString();
+        }
+
+        private void CheckWin(int tile)
+        {
+        }
+
+        private void SwitchState()
+        {
+            if (state == State.O)
+            {
+                state = State.X;
+            }
+            else
+            {
+                state = State.O;
+            }
         }
     }
 }
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairToe.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
